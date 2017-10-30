ace249fd23a0a971ec2a2515f2301888e8dec446
Lu <landcolin@gmail.com>
2017-10-30 10:41:47 +0800

    update

--- model/func_rang_bear_meas.m
+++ model/func_rang_bear_meas.m
@@ -1,14 +1,14 @@
-function  z = func_rang_bear_meas(pos, meas_param)
+function  z = func_rang_bear_meas(pos_tgt, meas_param)
 
-x_R = meas_param{1};
-y_R = meas_param{2};
+x = pos_tgt(1);
+y = pos_tgt(2);
 
-x = pos(1);
-y = pos(2);
+x_s = meas_param{1};
+y_s = meas_param{2};
 
-z_r = sqrt( (x - x_R)^2 + (y - y_R)^2);
-z_theta = atan2( (y - y_R), (x - x_R) );
+z_range = sqrt( (x - x_s)^2 + (y - y_s)^2);
+z_theta = atan2( (y - y_s), (x - x_s) );
 
 z = zeros(2, 1);
-z(1) = z_r;
+z(1) = z_range;
 z(2) = z_theta;
